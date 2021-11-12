e160ee06deda68fce79ca0f879c4b3467eeaf755
sergeliatko <serge@techspokes.com>
2021-11-12 12:38:54 +0100

    1.0.0 - major update.

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: serge
- * Date: 22/11/2018
- * Time: 18:03
- */
 
 namespace TechSpokes\ExcludeCategories;
+
+use WP_Query;
 
 /**
  * Class Plugin
@@ -15,16 +11,21 @@ namespace TechSpokes\ExcludeCategories;
  */
 class Plugin {
 
+	const SETTING_EXCLUDE_CATEGORIES_BLOG   = 'ts_exclude_categories_blog';
+	const SETTING_EXCLUDE_CATEGORIES_SEARCH = 'ts_exclude_categories_search';
+	const SETTING_EXCLUDE_CATEGORIES_FEED   = 'ts_exclude_categories_feed';
+
 	/**
 	 * @var \TechSpokes\ExcludeCategories\Plugin $instance
 	 */
 	protected static $instance;
 
 	/**
+	 * Initialize the plugin.
+	 *
 	 * @return \TechSpokes\ExcludeCategories\Plugin
 	 */
-	public static function getInstance() {
-
+	public static function getInstance(): Plugin {
 		if ( ! ( self::$instance instanceof Plugin ) ) {
 			self::setInstance( new self() );
 		}
@@ -33,26 +34,18 @@ class Plugin {
 	}
 
 	/**
+	 * Set the plugin instance.
+	 *
 	 * @param \TechSpokes\ExcludeCategories\Plugin $instance
 	 */
 	protected static function setInstance( Plugin $instance ) {
-
 		self::$instance = $instance;
-	}
-
-	/**
-	 * Loads plugin.
-	 */
-	public static function load() {
-
-		self::getInstance();
 	}
 
 	/**
 	 * Plugin constructor.
 	 */
 	protected function __construct() {
-
 		// register settings
 		add_action( 'init', array( $this, 'register_settings' ), 10, 0 );
 		// admin UI
@@ -65,34 +58,35 @@ class Plugin {
 	 * Registers settings in WordPress.
 	 */
 	public function register_settings() {
-
-		// blog
-		register_setting(
-			'reading',
-			'ts_exclude_categories_blog',
+		//register settings
+		foreach (
 			array(
-				'sanitize_callback' => array( $this, 'sanitize_setting' )
-			)
-		);
-
-		// feed
-		register_setting(
-			'reading',
-			'ts_exclude_categories_feed',
-			array(
-				'sanitize_callback' => array( $this, 'sanitize_setting' )
-			)
-		);
+				self::SETTING_EXCLUDE_CATEGORIES_BLOG,
+				self::SETTING_EXCLUDE_CATEGORIES_SEARCH,
+				self::SETTING_EXCLUDE_CATEGORIES_FEED,
+			) as $setting
+		) {
+			register_setting( 'reading', $setting, array( $this, 'sanitize_setting' ) );
+		}
 	}
 
 	/**
+	 * Sanitizes settings.
+	 *
 	 * @param string $value
 	 *
 	 * @return string
 	 */
-	public function sanitize_setting( $value = '' ) {
+	public function sanitize_setting( string $value = '' ): string {
+		//regex value to keep only numbers and commas
+		$value = preg_replace( '/[^0-9,]/', '', sanitize_text_field( $value ) );
+		//explode value into array and map to absint
+		$value = array_map( 'absint', explode( ',', $value ) );
+		//filter empty values and remove duplicates
+		$value = array_filter( array_unique( $value ) );
 
-		return trim( preg_replace( '/[,]{2,}/', ',', preg_replace( '/[^0-9,]/', '', sanitize_text_field( $value ) ) ), ',' );
+		//implode array back to string and return
+		return implode( ',', $value );
 	}
 
 	/**
@@ -108,7 +102,7 @@ class Plugin {
 			'reading'
 		);
 
-		// blog
+		//add settings field to exclude categories on main blog page
 		add_settings_field(
 			'techspokes-exclude-categories-blog',
 			__( 'Blog page', 'techspokes-exclude-categories' ),
@@ -116,19 +110,39 @@ class Plugin {
 			'reading',
 			'ts-exclude-categories',
 			array(
-				'label_for'        => 'ts-exclude-categories-blog',
+				'label_for'        => $this->sanitize_html_id( self::SETTING_EXCLUDE_CATEGORIES_BLOG ),
 				'description'      => __( 'Enter comma separated IDs of categories to exclude from blog page.', 'techspokes-exclude-categories' ),
 				'input_attributes' => array(
-					'id'    => 'ts-exclude-categories-blog',
-					'name'  => 'ts_exclude_categories_blog',
+					'id'    => $this->sanitize_html_id( self::SETTING_EXCLUDE_CATEGORIES_BLOG ),
+					'name'  => self::SETTING_EXCLUDE_CATEGORIES_BLOG,
 					'type'  => 'text',
-					'value' => get_option( 'ts_exclude_categories_blog' ),
-					'class' => 'widefat'
-				)
+					'value' => strval( get_option( self::SETTING_EXCLUDE_CATEGORIES_BLOG, '' ) ),
+					'class' => 'widefat',
+				),
 			)
 		);
 
-		// feed
+		//add settings field to exclude categories on search results page
+		add_settings_field(
+			'techspokes-exclude-categories-search',
+			__( 'Search results page', 'techspokes-exclude-categories' ),
+			array( $this, 'settings_field' ),
+			'reading',
+			'ts-exclude-categories',
+			array(
+				'label_for'        => $this->sanitize_html_id( self::SETTING_EXCLUDE_CATEGORIES_SEARCH ),
+				'description'      => __( 'Enter comma separated IDs of categories to exclude from search results page.', 'techspokes-exclude-categories' ),
+				'input_attributes' => array(
+					'id'    => $this->sanitize_html_id( self::SETTING_EXCLUDE_CATEGORIES_SEARCH ),
+					'name'  => self::SETTING_EXCLUDE_CATEGORIES_SEARCH,
+					'type'  => 'text',
+					'value' => strval( get_option( self::SETTING_EXCLUDE_CATEGORIES_SEARCH, '' ) ),
+					'class' => 'widefat',
+				),
+			)
+		);
+
+		//add setting field to exclude the categories in the feed
 		add_settings_field(
 			'techspokes-exclude-categories-feed',
 			__( 'Feed', 'techspokes-exclude-categories' ),
@@ -136,15 +150,15 @@ class Plugin {
 			'reading',
 			'ts-exclude-categories',
 			array(
-				'label_for'        => 'ts-exclude-categories-feed',
+				'label_for'        => $this->sanitize_html_id( self::SETTING_EXCLUDE_CATEGORIES_FEED ),
 				'description'      => __( 'Enter comma separated IDs of categories to exclude from feed.', 'techspokes-exclude-categories' ),
 				'input_attributes' => array(
-					'id'    => 'ts-exclude-categories-feed',
-					'name'  => 'ts_exclude_categories_feed',
+					'id'    => $this->sanitize_html_id( self::SETTING_EXCLUDE_CATEGORIES_FEED ),
+					'name'  => self::SETTING_EXCLUDE_CATEGORIES_FEED,
 					'type'  => 'text',
-					'value' => get_option( 'ts_exclude_categories_feed' ),
-					'class' => 'widefat'
-				)
+					'value' => strval( get_option( self::SETTING_EXCLUDE_CATEGORIES_FEED, '' ) ),
+					'class' => 'widefat',
+				),
 			)
 		);
 	}
@@ -155,7 +169,6 @@ class Plugin {
 	 * @param array $args
 	 */
 	public function settings_field( array $args ) {
-
 		/**
 		 * @var string $label_for
 		 * @var string $description
@@ -163,11 +176,11 @@ class Plugin {
 		 */
 		extract( $args, EXTR_OVERWRITE );
 		array_walk( $input_attributes, function ( &$item, $key ) {
-
 			$item = sprintf( '%1$s="%2$s"', $key, esc_attr( $item ) );
 		} );
+		/** @noinspection HtmlUnknownAttribute */
 		printf(
-			'<p><input %1$s /></p>',
+			'<p><input %1$s/></p>',
 			join( ' ', $input_attributes )
 		);
 		if ( ! empty( $description ) ) {
@@ -184,26 +197,73 @@ class Plugin {
 	 *
 	 * @param \WP_Query $query
 	 */
-	public function exclude_categories( \WP_Query &$query ) {
-
+	public function exclude_categories( WP_Query $query ) {
+		//only modify main query if it is not in the admin area
 		if ( $query->is_main_query() && ! $query->is_admin ) {
-			if ( $query->is_home() && ! $query->is_feed() ) {
-				$excluded = array_filter( array_map( 'absint', explode( ',', get_option( 'ts_exclude_categories_blog' ) ) ) );
-				if ( ! empty( $excluded ) ) {
-					$query->set( 'category__not_in', array_unique( array_merge(
-						(array) $query->get( 'category__not_in' ),
-						$excluded
-					) ) );
+			//if query is for main blog page
+			if ( $query->is_home() ) {
+				//is query a feed?
+				if ( $query->is_feed() ) {
+					//get excluded categories from feed
+					$excluded_categories = $this->get_excluded_categories( self::SETTING_EXCLUDE_CATEGORIES_FEED );
+				} else {
+					//get excluded categories from blog page
+					$excluded_categories = $this->get_excluded_categories( self::SETTING_EXCLUDE_CATEGORIES_BLOG );
 				}
-			} elseif ( $query->is_home() && $query->is_feed() ) {
-				$excluded = array_filter( array_map( 'absint', explode( ',', get_option( 'ts_exclude_categories_feed' ) ) ) );
-				if ( ! empty( $excluded ) ) {
-					$query->set( 'category__not_in', array_unique( array_merge(
-						(array) $query->get( 'category__not_in' ),
-						$excluded
-					) ) );
-				}
+			} elseif ( $query->is_search() ) {
+				//get excluded categories from search results page
+				$excluded_categories = $this->get_excluded_categories( self::SETTING_EXCLUDE_CATEGORIES_SEARCH );
+			} else {
+				//we have nothing to exclude
+				$excluded_categories = array();
+			}
+			//if there are categories to exclude
+			if ( ! empty( $excluded_categories ) ) {
+				//add category__not_in to existing query parameter
+				$query->set( 'category__not_in', array_filter( array_unique( array_merge(
+					(array) $query->get( 'category__not_in', array() ),
+					$excluded_categories
+				) ) ) );
 			}
 		}
 	}
+
+	/**
+	 * Sanitizes HTML ID attribute.
+	 *
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	protected function sanitize_html_id( string $name ): string {
+		//replace non literals or numbers with hyphens and return
+		return preg_replace( '/[^a-z0-9]+/i', '-', $name );
+	}
+
+	/**
+	 * Returns array excluded categories IDs.
+	 *
+	 * @param string $option
+	 *
+	 * @return array|int[]
+	 */
+	protected function get_excluded_categories( string $option ): array {
+		//return array of excluded categories IDs
+		return array_filter( array_unique( array_map(
+			'absint',
+			explode( ',', strval( get_option( $option, '' ) ) )
+		) ) );
+	}
+
+	/**
+	 * Checks if the provided value is empty.
+	 *
+	 * @param null $value
+	 *
+	 * @return bool
+	 */
+	protected function is_empty( $value = null ): bool {
+		return empty( $value );
+	}
+
 }
--- techspokes-exclude-categories.php
+++ techspokes-exclude-categories.php
@@ -11,7 +11,7 @@
  * Plugin Name: Exclude Categories by TechSpokes Inc.
  * Plugin URI:  https://github.com/TechSpokes/techspokes-exclude-categories.git
  * Description: Allows you to exclude specific categories from blog page and feed in WordPress.
- * Version:     0.0.1
+ * Version:     1.0.0
  * Author:      TechSpokes Inc.
  * Author URI:  https://techspokes.com
  * Text Domain: techspokes-exclude-categories
@@ -27,9 +27,9 @@ require_once( dirname( __FILE__ ) . '/autoload.php' );
 
 // load plugin text domain
 add_action( 'plugins_loaded', function () {
-
 	load_plugin_textdomain( 'techspokes-exclude-categories', false, basename( dirname( __FILE__ ) . '/languages' ) );
 }, 10, 0 );
 
 // load the plugin
-add_action( 'plugins_loaded', array( 'TechSpokes\ExcludeCategories\Plugin', 'load' ), 10, 0 );
+add_action( 'plugins_loaded', array( 'TechSpokes\ExcludeCategories\Plugin', 'getInstance' ), 10, 0 );
+
